d11c540b34f327ef302b252590dbd67ea2bd255a
rain18 <yumike18@126.com>
2016-10-02 11:14:55 +0800

    完善搜索json

--- search/js/cb-search.js
+++ search/js/cb-search.js
@@ -41,7 +41,7 @@
     toggleSearch(true);
   });
 
-  $.getJSON("/search/cb-search.json")
+  $.getJSON("/rain.github.io/search/cb-search.json")
     .done(function (data) {
       if (data.code == 0) {
         $("#search-content").typeahead({
